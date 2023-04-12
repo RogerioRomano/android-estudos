76c8a2c2b31206faa1d9bb046f91122369ce39e8
rogerioromano <rogerio_romano@hotmail.com>
2023-04-11 22:21:48 -0300

    log em controller salvar

--- AppListaCurso/app/src/main/java/com/devandroid/applistacurso/controller/PessoaController.java
+++ AppListaCurso/app/src/main/java/com/devandroid/applistacurso/controller/PessoaController.java
@@ -1,9 +1,11 @@
 package com.devandroid.applistacurso.controller;
 
+import android.util.Log;
+
 import com.devandroid.applistacurso.model.Pessoa;
 
 public class PessoaController {
     public void salvar(Pessoa pessoa) {
-
+        Log.i(getClass().getSimpleName(), pessoa.toString());
     }
 }
